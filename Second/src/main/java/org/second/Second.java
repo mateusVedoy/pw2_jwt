6440e0f0631a38768af82ce6bb89129178d8f05d
mateusVedoy <mateusvedoy@outlook.com>
2023-09-08 23:04:52 +0000

    feat: add auth to Second

--- Second/src/main/java/org/second/Second.java
+++ Second/src/main/java/org/second/Second.java
@@ -1,5 +1,10 @@
 package org.second;
 
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.Claims;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -9,8 +14,13 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/second")
 public class Second {
 
+    @Inject
+    @Claim(standard = Claims.email)
+    String email;
+
     @GET
     @Path("/getSum/{a}/{b}")
+    @RolesAllowed({"Admin"})
     @Produces(MediaType.TEXT_PLAIN)
     public int getSum(@PathParam("a") int a, @PathParam("b") int b){
         return a + b;
